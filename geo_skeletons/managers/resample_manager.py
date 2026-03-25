bce5901e6ca96fda7f2fd9046b6cc7de5743f1ae
Lenny HUCHER <lehuc2580@met.no>
2026-03-25 14:33:21 +0100

    add TODO

--- geo_skeletons/managers/resample_manager.py
+++ geo_skeletons/managers/resample_manager.py
@@ -6,6 +6,8 @@ from typing import Union, Optional
 from .resample.scipy_regridders import scipy_regridders
 import geo_parameters as gp
 from copy import deepcopy
+
+
 def squared_mean(x, *args, **kwargs):
     """Calculates root mean of squares. Used for averaging significant wave height"""
     return np.sqrt(np.mean(x**2, *args, **kwargs))
@@ -68,6 +70,7 @@ def set_up_mean_func(
     attr_str = f"{mode} mean {attr_str}"
     return mean_func, attr_str
 
+
 def find_original_skeleton_in_inheritance_chain(data):
     cls = data.__class__
     chain = [cls]
@@ -76,44 +79,45 @@ def find_original_skeleton_in_inheritance_chain(data):
         chain.append(cls)
     return chain[-3]
 
+
 def sort_out_regridded_type(data, new_grid) -> str:
     """Determines if we need to do gridded-gridded, point-gridded etc regridding"""
-    source = 'gridded' if data.is_gridded() else 'point'
-    target = 'gridded' if new_grid.is_gridded() else 'point'
+    source = "gridded" if data.is_gridded() else "point"
+    target = "gridded" if new_grid.is_gridded() else "point"
     return f"{source}_to_{target}"
 
 
 def create_new_class(data, new_grid):
-    """Creates a new class that will contain the gridded data. 
-    If we change type (i.e. from gridded to point), then we will reconstruct the class to contain the correct data variables etc"""
+    """Creates a new class that will contain the gridded data.
+    If we change type (i.e. from gridded to point), then we will reconstruct the class to contain the correct data variables etc
+    """
     if data.is_gridded() and new_grid.is_gridded():
         return data.__class__
     if not data.is_gridded() and not new_grid.is_gridded():
         return data.__class__
 
-    
     old_base = find_original_skeleton_in_inheritance_chain(new_grid)
     if new_grid.is_gridded():
-        new_base = type(f'Gridded{data.__class__.__name__}', (old_base,), {})
+        new_base = type(f"Gridded{data.__class__.__name__}", (old_base,), {})
     else:
-        new_base = type(f'Point{data.__class__.__name__}', (old_base,), {})
+        new_base = type(f"Point{data.__class__.__name__}", (old_base,), {})
 
     for key, param in data.core._added_coords.items():
-        if key == 'time':
-            new_base=new_base.add_time()
-        elif key in ['x','y','lon','lat','inds']:
+        if key == "time":
+            new_base = new_base.add_time()
+        elif key in ["x", "y", "lon", "lat", "inds"]:
             continue
         elif gp.wave.Freq.is_same(param.meta):
             new_base = new_base.add_frequency(param)
         elif gp.wave.Dirs.is_same(param.meta):
             new_base = new_base.add_direction(param)
-        else:   
+        else:
             new_base = new_base.add_coord(param)
 
     for key, param in data.core._added_vars.items():
-        if key not in ['x','y','lon','lat']:
+        if key not in ["x", "y", "lon", "lat"]:
             new_base = new_base.add_datavar(param)
-    
+
     for key, param in data.core._added_magnitudes.items():
         direction = param.direction
         if direction is not None:
@@ -121,8 +125,14 @@ def create_new_class(data, new_grid):
             direction = direction.meta or direction.name
         else:
             direction, dir_type = None, None
-        new_base = new_base.add_magnitude(param.meta or param.name, x=param.x, y=param.y, direction=direction, dir_type=dir_type)
-           
+        new_base = new_base.add_magnitude(
+            param.meta or param.name,
+            x=param.x,
+            y=param.y,
+            direction=direction,
+            dir_type=dir_type,
+        )
+
     ignore_these = []
     for key, param in data.core._added_masks.items():
         opposite_mask = param.opposite_mask
@@ -132,31 +142,38 @@ def create_new_class(data, new_grid):
         else:
             opposite_name = None
         if (param.meta or param.name[:-5]) not in ignore_these:
-            new_base = new_base.add_mask(param.meta or param.name[:-5], default_value=param.default_value, coord_group=param.coord_group, opposite_name=opposite_name, triggered_by=param.triggered_by, valid_range=param.valid_range, range_inclusive=param.range_inclusive)
-    
+            new_base = new_base.add_mask(
+                param.meta or param.name[:-5],
+                default_value=param.default_value,
+                coord_group=param.coord_group,
+                opposite_name=opposite_name,
+                triggered_by=param.triggered_by,
+                valid_range=param.valid_range,
+                range_inclusive=param.range_inclusive,
+            )
 
     new_base_coords = new_base.core._added_coords
     new_base_vars = new_base.core._added_vars
-    new_base.core = deepcopy(data.__class__.core) # Copy over coordinates, data variables, magnitudes, masks etc.
+    new_base.core = deepcopy(
+        data.__class__.core
+    )  # Copy over coordinates, data variables, magnitudes, masks etc.
     if not data.is_gridded() and new_grid.is_gridded():
-        del new_base.core._added_coords['inds']
-        del new_base.core._added_vars['x']
-        del new_base.core._added_vars['y']
-        new_base.core._added_coords['x'] = new_base_coords['x']
-        new_base.core._added_coords['y'] = new_base_coords['y']
+        del new_base.core._added_coords["inds"]
+        del new_base.core._added_vars["x"]
+        del new_base.core._added_vars["y"]
+        new_base.core._added_coords["x"] = new_base_coords["x"]
+        new_base.core._added_coords["y"] = new_base_coords["y"]
 
-        
         return new_base
 
     if data.is_gridded() and not new_grid.is_gridded():
-        del new_base.core._added_coords['x']
-        del new_base.core._added_coords['y']
+        del new_base.core._added_coords["x"]
+        del new_base.core._added_coords["y"]
 
-        new_base.core._added_coords['inds'] = new_base_coords['inds']
-        new_base.core._added_vars['x'] = new_base_vars['x']
-        new_base.core._added_vars['y'] = new_base_vars['y']
+        new_base.core._added_coords["inds"] = new_base_coords["inds"]
+        new_base.core._added_vars["x"] = new_base_vars["x"]
+        new_base.core._added_vars["y"] = new_base_vars["y"]
 
-        
         return new_base
 
 
@@ -166,50 +183,64 @@ def init_new_class_to_grid(new_class, new_grid, data):
     # This is a hack. Make it better later
     new_lon, new_lat = new_grid.lon(native=True), new_grid.lat(native=True)
     new_coords = {new_grid.core.x_str: new_lon, new_grid.core.y_str: new_lat}
-    
+
     for coord in data.core.coords():
-        if coord not in ['x','y','lon','lat','inds']:
+        if coord not in ["x", "y", "lon", "lat", "inds"]:
             new_coords[coord] = data.get(coord)
-       
+
     new_data = new_class(**new_coords)
     if new_data.core.is_cartesian():
-        new_data.utm.set(data.utm.zone(), silent=True)
+        new_data.utm.set(data.utm.zone(), silent=True)  # TODO switch utm to proj
 
     return new_data
 
-REGRID_ENGINES = {'scipy': scipy_regridders}
+
+REGRID_ENGINES = {"scipy": scipy_regridders}
+
 
 class ResampleManager:
     def __init__(self, skeleton):
         self.skeleton = skeleton
 
-
     def engines(self):
         """Lists engines that are available for regridding"""
-        print('-'*125)
-        print('Engine\t\t\tgrid-to-grid\tpoint-to-grid\tpoint-to-point\tgrid-to-point\tInstallation')
-        print('-'*125)
+        print("-" * 125)
+        print(
+            "Engine\t\t\tgrid-to-grid\tpoint-to-grid\tpoint-to-point\tgrid-to-point\tInstallation"
+        )
+        print("-" * 125)
         for key, value in REGRID_ENGINES.items():
-            g2g = 'Yes' if value.get('gridded_to_gridded') is not None else 'No'
-            p2g = 'Yes' if value.get('point_to_gridded') is not None else 'No'
-            p2p = 'Yes' if value.get('point_to_point') is not None else 'No'
-            g2p = 'Yes' if value.get('gridded_to_point') is not None else 'No'
-            
-            available = 'Installed' if value.get('available') else 'Not installed'
+            g2g = "Yes" if value.get("gridded_to_gridded") is not None else "No"
+            p2g = "Yes" if value.get("point_to_gridded") is not None else "No"
+            p2p = "Yes" if value.get("point_to_point") is not None else "No"
+            g2p = "Yes" if value.get("gridded_to_point") is not None else "No"
 
-            print(f"'{key}' ({available})\t     {g2g}\t      {p2g}\t      {p2p}\t     {g2p}\t{value.get('installation')}")
-        print('-'*125)
-        print('Engine\t\tOptions')
-        print('-'*125)
+            available = "Installed" if value.get("available") else "Not installed"
+
+            print(
+                f"'{key}' ({available})\t     {g2g}\t      {p2g}\t      {p2p}\t     {g2p}\t{value.get('installation')}"
+            )
+        print("-" * 125)
+        print("Engine\t\tOptions")
+        print("-" * 125)
         for key, value in REGRID_ENGINES.items():
-            
+
             print(f"'{key}'\t\t{value.get('options')}")
-        print('-'*125)
+        print("-" * 125)
 
-    def grid(self, new_grid, target_class = None, engine: str='scipy', verbose: bool=True, **kwargs):
+    def grid(
+        self,
+        new_grid,
+        target_class=None,
+        engine: str = "scipy",
+        verbose: bool = True,
+        **kwargs,
+    ):
         """Regrids the data of the skeleton to a new grid"""
         if engine not in REGRID_ENGINES.keys():
-            raise ValueError(f"'engine' needs to be in {list(REGRID_ENGINES.keys())}, not '{engine}'!")
+            raise ValueError(
+                f"'engine' needs to be in {list(REGRID_ENGINES.keys())}, not '{engine}'!"
+            )
 
         regridder_dict = REGRID_ENGINES.get(engine)
         regrid_type = sort_out_regridded_type(self.skeleton, new_grid)
@@ -217,23 +248,34 @@ class ResampleManager:
         regridder = regridder_dict.get(regrid_type)
 
         if regridder is None:
-            raise NotImplementedError(f"'{regrid_type}' regridding not available for engine '{engine}'")
+            raise NotImplementedError(
+                f"'{regrid_type}' regridding not available for engine '{engine}'"
+            )
 
         new_class = target_class or create_new_class(self.skeleton, new_grid)
         new_data = init_new_class_to_grid(new_class, new_grid, self.skeleton)
 
         if verbose:
-            print(f"Original data has spatial coords {self.skeleton.core.coords('spatial')}")
+            print(
+                f"Original data has spatial coords {self.skeleton.core.coords('spatial')}"
+            )
             if new_data.core.is_cartesian():
-                print(f"Target grid has spatial coords {new_data.core.coords('spatial')} UTM {new_data.utm.zone()}")
+                print(
+                    f"Target grid has spatial coords {new_data.core.coords('spatial')} UTM {new_data.utm.zone()}"
+                )
             else:
-                print(f"Target grid has spatial coords {new_data.core.coords('spatial')}")
+                print(
+                    f"Target grid has spatial coords {new_data.core.coords('spatial')}"
+                )
 
-            
-            print(f"Starting regridding ('{regrid_type}') with '{engine}'({regridder})...")
-        
-        new_data = regridder(self.skeleton, new_grid, new_data, verbose=verbose, **kwargs)
-        
+            print(
+                f"Starting regridding ('{regrid_type}') with '{engine}'({regridder})..."
+            )
+
+        new_data = regridder(
+            self.skeleton, new_grid, new_data, verbose=verbose, **kwargs
+        )
+
         return new_data
 
     def time(
